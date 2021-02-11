2e723c37891d03bbc2ddc496c56f8a5e4aabfa70
arpan-jo <arpanjorng@gmail.com>
2021-02-11 15:52:59 +0600

    music lyric

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const getMusic = () => {
    const music = document.getElementById('inputName').value;
-   fetch(`https://api.lyrics.ovh/suggest/${music}`)
-      .then(res => res.json())
-      .then(data => getAlbum(data.data));
+   if (music === '') {
+      alert('type the song name that you want to listen');
+      document.getElementById('container').innerHTML = '';
+   } else {
+      fetch(`https://api.lyrics.ovh/suggest/${music}`)
+         .then(res => res.json())
+         .then(data => getAlbum(data.data))
+         .catch(error => console.log(error));
+   }
    document.getElementById('inputName').value = '';
 };
 
@@ -12,7 +18,7 @@ const getAlbum = album => {
    album.forEach(element => {
       const pot = document.createElement('div');
       pot.className = 'single-result row align-items-center my-3 p-3';
-      const container2 = ` 
+      const container2 = `
         <div class="col-md-9">
           <h3 id="musicName" class="lyrics-name">
              ${element.title}
@@ -34,12 +40,14 @@ const getAlbum = album => {
    });
 };
 
-const getLyric = (singerName, musicName) => {
-   fetch(`https://api.lyrics.ovh/v1/${singerName}/${musicName}`)
-      .then(res => res.json())
-      .then(data => {
-         if (data.lyrics === '') {
-            alert('NO lyrics in this song');
-         } else alert(data.lyrics);
-      });
+const getLyric = async (singerName, musicName) => {
+   const res = await fetch(
+      `https://api.lyrics.ovh/v1/${singerName}/${musicName}`
+   );
+   const data = await res.json();
+   if (data.lyrics === '') {
+      alert('NO lyrics in this song');
+   } else {
+      alert(data.lyrics);
+   }
 };
